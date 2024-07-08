1358930a0dca564d232d16886cdbb7a5e9fe339c
Wanderson Silva <ca-scudo@hotmail.com>
2024-07-08 00:15:52 -0300

    Update Consultas prontas.sql

--- Consultas prontas.sql
+++ Consultas prontas.sql
@@ -1,5 +1,12 @@
 //Consultas SQL Prontas
 
+//Fitro por cidade
 select CustomerID as "ID Cliente", CustomerName as "Nome Cliente", Country as "País", City as "Cidade"
 from Customers
 WHERE City = 'São Paulo'
+
+//Ordenar de forma acendente por cidade
+SELECT CustomerID, CustomerName, Country
+FROM Customers
+ORDER BY Country;
+
