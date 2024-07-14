ef26e4e714362759e53458a708e2f573c104f2e0
Wanderson Silva <ca-scudo@hotmail.com>
2024-07-14 15:21:39 -0300

    Update 02 - Consultas prontas - Fun. Matématicas.sql

--- 02 - Consultas prontas - Fun. Matématicas.sql
+++ 02 - Consultas prontas - Fun. Matématicas.sql
@@ -1,2 +1,8 @@
 //Consultas SQL Prontas.
 // Base de consultas: https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all&comments=1
+
+//Contar o numero de cietes
+SELECT COUNT(*) as Numero_clientes
+FROM Customers;
+
+
