c2252f3ca3e4bb7df8528f62912f81725dc3a4c7
Wanderson Silva <ca-scudo@hotmail.com>
2024-07-17 05:44:02 -0300

    Update 04 - Consultas prontas - JOIN , N e Sub-querysParte 2.sql

--- 04 - Consultas prontas - JOIN , N e Sub-querysParte 2.sql
+++ 04 - Consultas prontas - JOIN , N e Sub-querysParte 2.sql
@@ -2,3 +2,8 @@
 // Base de consultas: https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_all&comments=1
 
 
+//Listar a quantidade de pedido/vendas por cliente. Usando JOIN E GROUP BY
+SELECT [Production].[Product].Name, SUM([Sales].[SalesOrderDetail].OrderQty) as 'Qtd. de compras'
+FROM [Production].[Product]
+	JOIN [Sales].[SalesOrderDetail] ON [Sales].[SalesOrderDetail].SalesOrderDetailID = [Production].[Product].ProductID
+	GROUP BY [Production].[Product].Name 
