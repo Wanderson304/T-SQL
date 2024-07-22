93c6ed46f50749f3826e17db9d8fc42b45891acc
Wanderson Silva <ca-scudo@hotmail.com>
2024-07-22 05:20:07 -0300

    Update 04 - Consultas prontas - JOIN , N e Sub-querysParte 2.sql

--- 04 - Consultas prontas - JOIN , N e Sub-querysParte 2.sql
+++ 04 - Consultas prontas - JOIN , N e Sub-querysParte 2.sql
@@ -1,7 +1,7 @@
 //Consultas SQL Prontas.
+	
+
 // Base de consultas: AdventureWorks2016 (SQL server Local)
-
-
 //Listar a quantidade de pedido/vendas por cliente. Usando JOIN E GROUP BY
 SELECT [Production].[Product].Name, SUM([Sales].[SalesOrderDetail].OrderQty) as 'Qtd. de compras'
 FROM [Production].[Product]
